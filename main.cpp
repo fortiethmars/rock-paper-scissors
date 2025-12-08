dd9a7b9d0bb8d958fb05a21dda4fee4b7772063a
ghost-mann <iamaob8@protonmail.com>
2025-12-08 13:23:54 +0300

    feat: added randomised computer choice in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <string>
-#include <cctype>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
     string playerChoice;
+    string computerChoice;
     string choices[3] = {"rock","paper","scissors"};
+
+    srand(time(0));
+    int randomNum = rand() % 3;
+    computerChoice = choices[randomNum];
+
 
     cout << "Welcome to Rock,Paper,Scissors!\n";
     cout << "Please choose one (Rock,Paper or Scissors): ";
 
     cin >> playerChoice;
+
+
 
     bool isValid = false;
     if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
@@ -21,7 +30,10 @@ int main() {
         cout << "Your entry is invalid, please try again!\n";
     } else {
         cout << "You chose: " << playerChoice << endl;
+        cout << "Computer chose: " << computerChoice << endl;
     }
-        
+    
+    
+    
 
 }
